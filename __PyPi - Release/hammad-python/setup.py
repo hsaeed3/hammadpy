ff268fb878fd179c09134b6148ba018aa35790bf
hammad <hsaeed3@student.ohlone.edu>
2024-03-15 11:16:51 -0700

    Push Dist

--- __PyPi - Release/hammad-python/setup.py
+++ __PyPi - Release/hammad-python/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hammadpy",
-    version="3.0.0",
+    version="3.0.1",
     author="Hammad Saeed",
     author_email="hammad@supportvectors.com",
     description="Hammad's Accelarated Micro Modules for Application Development (Hammad's Python Library)",
